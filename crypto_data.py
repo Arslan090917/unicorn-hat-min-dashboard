3ac3e1424c51fcb2cea54eff1666d8254f28e8b4
Arslan090917 <ars-dolgaev@yandex.ru>
2023-03-06 20:07:54 +0600

    fix

---
+++ crypto_data.py
@@ -0,0 +1,54 @@
+'''
+Get data from coinmarketcup
+'''
+import json
+import logging
+import requests
+
+from config import Config
+
+class Coinmarketcup:
+    def __init__(self) -> None:
+        self.session = requests.Session()
+        self.endpoint = '/v1/cryptocurrency/listings/latest'
+        self.params = {'start': 1, 'limit': 1000, 'convert': 'USD'}
+        self.headers = {
+            "X-CMC_PRO_API_KEY" : Config.COINT_MARKET_API_KEY,
+            "Content-type": "application/json"
+        }
+        self.data = None
+        self.crypto_names = ['BTC', 'HFT']
+    
+    def get_pirce(self, response) -> dict:
+        '''
+        Function for geting price from json response
+        :params *crypto_name: dict - take list of crypto currency 
+        Example: ['BTC', 'ETH', 'HFT']
+        '''
+        # price = json.loads(self.response)['data'][0]['quote']['USD']['price']
+        crypto_data = dict()
+        json_data = json.loads(response)
+        for name in self.crypto_names:
+            for currency in json_data['data']:
+                if name == currency['symbol']:
+                    crypto_data[name] = str(currency['quote']['USD']['price'])[:6]
+        return crypto_data
+
+    def update_crypto_data(self) -> dict:
+        try:
+            self.response = self.session.get(
+                Config.COINT_MARKET_URL + self.endpoint, headers= self.headers, params=self.params)
+            logging.info(f'Send request {Config.COINT_MARKET_URL}', extra= {'Status code': self.response.status_code})
+            if self.response.status_code in [200, 201]:
+                try:
+                    self.data = self.get_pirce(self.response.text)
+                    logging.info('Update crypto data', extra={'BTC': self.data['BTC'], 'HFT': self.data['HFT']})
+                except Exception as exp:
+                    logging.error(exp)
+                    raise exp
+                logging.info(f'Finish updating crypto currency data')
+            else:
+                logging.error(f'ERROR Request to {Config.COINT_MARKET_URL} Staus code: {self.response.status_code}')
+        except Exception as exp:
+            logging.error(f'{exp} error')
+            raise exp        
---
+++ main.py
@@ -0,0 +1,116 @@
+import random
+import logging
+import asyncio
+import json
+import time
+import sys
+import math
+import asyncio
+
+from datetime import datetime
+from colorsys import hsv_to_rgb
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from unicornhatmini import UnicornHATMini
+
+from config import Config
+from crypto_data import Coinmarketcup
+
+class Main:
+    def __init__(self) -> None:
+        self.unicorn = UnicornHATMini()
+        self.unicorn.set_brightness(0.1)
+        self.display_width, self.display_height = self.unicorn.get_shape()
+        self.text_width, self.text_height = (0 ,0)
+        self.font = ImageFont.truetype("/home/pi/unicorn-hat-min-dashboard/5x7.ttf", 8)
+        self.coin = Coinmarketcup()
+        self.coin.update_crypto_data() # update price before start
+        self.date = datetime.now()
+        self.text = 'DEFAULT'
+        self.image = None
+        self.count = 0
+
+    def create_text(self) -> None:
+        self.coin.update_crypto_data()
+        self.text = f'{datetime.strftime(self.date, "%H%M")} || BTC:{self.coin.data["BTC"]} || HFT:{self.coin.data["HFT"]}'
+        print('update text')
+    
+    def create_image(self) -> None:
+        self.create_text()
+        self.text_width, self.text_height = self.font.getsize(self.text)
+        # Create a new PIL image big enough to fit the text
+        self.image = Image.new('P', (
+            self.text_width + self.display_width + self.display_width, self.display_height), 0)
+        self.draw = ImageDraw.Draw(self.image)
+        # self.set_rotation()
+        # Draw the text into the image
+        self.draw.text((self.display_width, -1), self.text, font=self.font, fill=255)
+        print('create images')
+
+    #print text on led panel       
+    async def display_info(self) -> None:
+        print('Start')
+        self.create_image()
+        offset_x = 0
+        a = True
+        while a:
+            # self.create_text()
+            for y in range(self.display_height):
+                for x in range(self.display_width):
+                    # if end of image set zero offset
+                    # hue = (time.time() / 10.0) + (x / float(self.display_width * 2))
+                    # r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
+                    r, g, b = (255, 255, 255)
+                    try:
+                        self.image.getpixel((x + offset_x, y))
+                    except Exception:
+                        offset_x = 0
+                    if self.image.getpixel((x + offset_x, y)) == 255:
+                        test = self.image
+                        self.unicorn.set_pixel(x, y, r, g, b)
+                    else:
+                        self.unicorn.set_pixel(x, y, 0, 0, 0)
+            
+            offset_x += 1
+            # if end of image set zero offset
+            if offset_x + self.display_height + 1 >= self.image.size[0]:
+                offset_x = 0
+            self.unicorn.show()
+            # a = False
+            await asyncio.sleep(1)
+
+    async def minute_pass(self):
+        while True:
+            if self.date.minute != datetime.now().minute:
+                self.date = datetime.now()
+                print('Updated text SUCCESS')
+                print(f'{self.text}')
+                self.create_image()
+                self.count += 1
+            else:
+                print('await 3 sec update_text()')
+                await asyncio.sleep(1)
+
+    async def update_cryptocurrency_data(self):
+        while True:
+            if self.count == 25:
+                self.coin.update_crypto_data()
+                print('print updated price')
+                logging.warning('INFO system get data from coinmarketcup every 20 minutes')
+                self.count = 0
+            else:
+                await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    dash = Main()
+    io_loops = asyncio.get_event_loop()
+    task = [
+        io_loops.create_task(dash.display_info()),
+        io_loops.create_task(dash.minute_pass()),
+        io_loops.create_task(dash.update_cryptocurrency_data()),
+    ]
+    io_loops.run_until_complete(asyncio.wait(task))
+    io_loops.close()
+    
